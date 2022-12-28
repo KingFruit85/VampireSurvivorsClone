c268abb16065b580264ba5bd40778631119e13e0
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2022-12-28 17:23:34 +0000

    added basic mob movement

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    Vector3 PlayerPosition;
+    float Speed = 2.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = Vector3.MoveTowards(transform.position, PlayerPosition, Speed * Time.deltaTime);
+    }
+}
