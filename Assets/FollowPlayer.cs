3b2a28a01fe4267762f782225eb7fdf30f5b3651
Christopher Long <36999273+KingFruit85@users.noreply.github.com>
2022-12-28 13:48:43 +0000

    created a basic game world using a tile map/ tile pallet

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private Vector3 PlayerPosition;
+    private Camera Camera;
+
+    void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        PlayerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+        Camera.transform.position = PlayerPosition;
+
+    }
+}
